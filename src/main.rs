5fd8e02aff3fe5105413deb49d979ef39f6f74bd
SemyonSinchenko <semyonsinchenko@yahoo.com>
2023-11-01 13:15:15 +0100

    try to pass 2d stage

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,15 @@ use std::{
 };
 
 fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+    println!("inside a function handle_connection");
     let res = "HTTP/1.1 200 OK\r\n\r\n";
     BufWriter::new(&stream).write_all(res.as_bytes()).unwrap();
     stream.flush()
 }
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
