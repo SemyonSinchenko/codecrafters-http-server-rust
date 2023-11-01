8303586762de8d46c79c4ad876cece6f9a8bd3f6
SemyonSinchenko <semyonsinchenko@yahoo.com>
2023-11-01 13:10:06 +0100

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{net::{TcpListener, TcpStream}, io::{BufWriter, Write}};
+
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
+    let res = "HTTP/1.1 200 OK\r\n\r\n";
+    BufWriter::new(&stream).write_all(res.as_bytes()).unwrap();
+    stream.flush()
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,6 +18,10 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                match handle_connection(_stream) {
+                    Ok(_r) => println!("sent a response"),
+                    Err(e) => println!("error happened: {}", e)
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
