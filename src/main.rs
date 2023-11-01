adff614dd933a8b97d8b696238ef20a5cede29ec
SemyonSinchenko <semyonsinchenko@yahoo.com>
2023-11-01 14:57:56 +0100

    add parsing of request

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,49 @@
 // Uncomment this block to pass the first stage
 use std::{
-    io::{BufWriter, Write},
+    io::{BufRead, BufReader, BufWriter, Write},
     net::{TcpListener, TcpStream},
 };
 
-fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
-    println!("inside a function handle_connection");
-    let res = "HTTP/1.1 200 OK\r\n\r\n";
-    BufWriter::new(&stream).write_all(res.as_bytes()).unwrap();
-    stream.flush()
+fn parse_request(stream: &TcpStream) -> Result<String, String> {
+    let mut request_reader = BufReader::new(stream);
+    let mut request: String = Default::default();
+
+    if request_reader.read_line(&mut request).is_err() {
+        return Err("error parsing request".to_string());
+    }
+
+    if request.is_empty() {
+        return Err("empty string was passed".to_string());
+    }
+
+    let words: Vec<&str> = request.split_ascii_whitespace().collect();
+
+    let (command, arg) = (words.get(0).unwrap_or(&""), words.get(1).unwrap_or(&""));
+
+    match *command {
+        "GET" => {
+            let response = if arg.starts_with("/") {
+                "HTTP/1.1 200 OK\r\n\r\n"
+            } else {
+                "HTTP/1.1 404 Not Found\r\n\r\n"
+            };
+            Ok(response.to_string())
+        }
+        s => Err(format!("unknown command {}", s)),
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<(), String> {
+    let response = parse_request(&stream);
+
+    match response {
+        Ok(_s) => {
+            BufWriter::new(&stream).write_all(_s.as_bytes()).unwrap();
+            let _ = stream.flush();
+            Ok(())
+        }
+        Err(e) => Err(e),
+    }
 }
 
 fn main() {
